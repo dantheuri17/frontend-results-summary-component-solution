81f4c26a51e177a9024183930f045ac7ff0cdf6b
Daniel Theuri <dantheuri17@gmail.com>
2023-12-22 20:56:38 +0300

    removing footer

--- src/App.tsx
+++ src/App.tsx
@@ -93,13 +93,6 @@ function App() {
 				</div>
 			</div>
 
-			<div className="attribution">
-				Challenge by{" "}
-				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-					Frontend Mentor
-				</a>
-				. Coded by <a href="#">Your Name Here</a>.
-			</div>
 		</>
 	);
 }
